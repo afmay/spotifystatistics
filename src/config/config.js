7011f3dd9d7478c874e6b919ad6a3f1953f2396b
afmay <71290938+afmay@users.noreply.github.com>
2021-04-28 12:52:26 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ const config = {
     client_id: 'b9b1bc33ef1746b0b394e0333f15f036',
     redirect_uri: `${origin}/spotifycallback`,
     scope:
-      'user-read-private user-read-recently-played user-read-currently-playing playlist-modify-public playlist-modify-private user-read-play-history',
+      'user-read-private user-top-read user-read-recently-played user-read-currently-playing playlist-modify-public playlist-modify-private user-read-play-history',
     show_dialog: true,
   },
 };
