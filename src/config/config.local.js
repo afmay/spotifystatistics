d3ade100a3f55b4e6682076fe399f031d48a423e
afmay <71290938+afmay@users.noreply.github.com>
2021-04-29 14:16:20 -0700

    Update config.local.js

--- src/config/config.local.js
+++ src/config/config.local.js
@@ -2,7 +2,7 @@ const config = {
   environment: 'development',
   remoteUrl: 'https://api.spotify.com/v1/',
   spotifyAuthparams: {
-    client_id: 'b9b1bc33ef1746b0b394e0333f15f036',
+    client_id: '91267676570e417f9dd68f2aaea7f0d9',
     redirect_uri: `${origin}/spotifycallback`,
     scope:
       'user-read-private user-top-read user-read-recently-played user-read-currently-playing playlist-modify-public playlist-modify-private user-read-play-history',
